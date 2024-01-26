10516a2c729c796c5952637a66f48a3ea5970e3b
dohieu1133 <dohieu1133@gmail.com>
2024-01-26 23:05:18 +0700

    volume

--- Assets/Scripts/SetSoundParent.cs
+++ Assets/Scripts/SetSoundParent.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SetParent : MonoBehaviour
+public class SetSoundParent : MonoBehaviour
 {
     private void Start()
     {
         if (transform.parent != null)
         transform.SetParent(transform.parent.parent);
+
+        AudioSource sound = GetComponent<AudioSource>();
+        if (sound != null)
+        {
+            sound.volume = PlayerPrefs.GetFloat("Volume", 1f);
+        }
     }
 }
